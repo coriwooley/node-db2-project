43aa94418467c453d2f6dcacff8ca0b1a3e0d142
Cori Wooley <wooleycori@gmail.com>
2022-06-14 02:34:39 -0800

    cars knex seed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,34 @@
-// STRETCH
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+
+const cars = [
+        {vin: '11111111111111111', 
+        make: 'ford',
+        model: 'F-150',
+        mileage: 300000,
+        title: 'clean',
+        transmission: 'automatic',
+    
+    },
+        {vin: '11111111111111111', 
+        make: 'honda',
+        model: 'civic',
+        mileage: 200000,
+        title: 'lien',
+    
+    },
+        {vin: '11111111111111111', 
+        make: 'chrysler',
+        model: 'town and country',
+        mileage: 100000
+    
+    }
+    ]
+
+ exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars);
+  };
+  
